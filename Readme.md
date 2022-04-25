25b9db74f0913472534707e8b5bb2b4bf5c4cba0
ABHISHEK UPADHYAY <abhisheku722@gmail.com>
2022-04-25 13:27:53 +0530

    readme and hindi ocr tested
    test successful

---
+++ Readme.md
@@ -0,0 +1,11 @@
+
+# OCR Example with OpenCV, Python and Tesseract
+
+<p>Here in this example we have, installed Tesseract from [here](https://github.com/UB-Mannheim/tesseract/wiki) and after that we have just used its installation path inside the program.
+
+Other than these you might need to install opencv-python and pytesseract packages in your venv. 
+
+Just for the reference though, I have also put the requirements.txt file.
+
+
+This is completely open source.</p>
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import cv2
 import pytesseract as pyt
-
+#declaring path of tesseract installation
 pyt.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
+#declaring image file
 img = cv2.imread('test.jpg')
-img = cv2.resize(img, (500, 500))
+# img = cv2.resize(img, (1366, 768))
 
+# showing image file
 cv2.imshow('image', img)
-text = pyt.image_to_string(img)
+
+#declaring text variable that converts image to text
+text=pyt.image_to_string(img, lang='hin')
+#prints text for preview in terminal
 print(text)
+
+# This writes the result to a text file
+with open('file.txt', mode='w') as f:
+    f.write(text)
+
+#standard waitKey method
 cv2.waitKey(0)
 cv2.destroyAllWindows()
